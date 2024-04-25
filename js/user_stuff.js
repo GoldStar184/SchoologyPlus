d0efa537b3deb0399c81033ff8740bdb7ea379b2
geppp41 <egep41414141@gmail.com>
2024-04-25 07:34:30 -0700

    created user_stuff.js to get users classes  id and titles

---
+++ js/user_stuff.js
@@ -0,0 +1,33 @@
+(async function () {
+    // Wait for loader.js to finish running
+    while (!window.splusLoaded) {
+        await new Promise(resolve => setTimeout(resolve, 10));
+    }
+    await loadDependencies("user_stuff", ["preload"]);
+})();
+
+var user_courses;
+var course_ids = []
+var course_titles = []
+//frame = document.querySelector("iframe[src*=session-tracker]").src.split("?")[1].split("=")[1]
+async function get_courses(){
+    user_courses = (await fetchApiJson(`/users/${getUserId()}/sections`)).section
+}
+async function get_course_ids(){
+    for(i = 0; i < 14; i++){
+        course_ids.push(fetchCourseId(i))
+        course_titles.push(fetchCourseTitle(i))
+    }
+}
+async function fetchCourseId(index){
+    return parseInt((await fetchApiJson(`/users/${getUserId()}/sections`)).section[index].id)
+}
+async function fetchCourseTitle(index){
+    return (await fetchApiJson(`/users/${getUserId()}/sections`)).section[index].course_title
+}
+window.addEventListener('load', function () {
+    get_courses()
+    get_course_ids()
+    console.log(course_ids)
+    console.log(course_titles)
+})
