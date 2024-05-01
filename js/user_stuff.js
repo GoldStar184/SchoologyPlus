93f664eb4f0a2cdbdb9f8fb89e40e2f662ad8ef2
geppp41 <egep41414141@gmail.com>
2024-05-01 13:32:22 -0700

    grabs users classes and puts them in array
    might not be correct just how i explain

--- js/user_stuff.js
+++ js/user_stuff.js
@@ -9,14 +9,15 @@
 var user_courses;
 var course_ids = []
 var course_titles = []
+var somethinging = []
 //frame = document.querySelector("iframe[src*=session-tracker]").src.split("?")[1].split("=")[1]
 async function get_courses(){
     user_courses = (await fetchApiJson(`/users/${getUserId()}/sections`)).section
 }
 async function get_course_ids(){
     for(i = 0; i < 14; i++){
-        course_ids.push(fetchCourseId(i))
-        course_titles.push(fetchCourseTitle(i))
+        course_ids.push(await fetchCourseId(i))
+        course_titles.push(await fetchCourseTitle(i))
     }
 }
 async function fetchCourseId(index){
@@ -26,12 +27,18 @@ async function fetchCourseTitle(index){
     return (await fetchApiJson(`/users/${getUserId()}/sections`)).section[index].course_title
 }
 async function something(){
-    return Array((await fetchApiJson(`/users/${getUserId()}/sections`)).section)
+    return await fetchApiJson(`/users/${getUserId()}/sections`).then((data) => {
+        return data.section;
+    })    //return await Promise.all((await fetchApiJson(`/users/${getUserId()}/sections`)).section)
+
 }
-window.addEventListener('load', function () {
-    console.log(something())
-    get_courses()
-    get_course_ids()
+window.addEventListener('load', async function () {
+    somethinging = await something()
+
+    console.log(somethinging)
+    console.log(somethinging[0])
+    await get_courses()
+    await get_course_ids()
     console.log(course_ids)
     console.log(course_titles)
 })
