ac72fc4b99d84c2b3938fae175ca5a2412b1ff27
geppp41 <egep41414141@gmail.com>
2024-05-02 13:25:41 -0700

    fixed course ids and titles not being stored

--- js/user_stuff.js
+++ js/user_stuff.js
@@ -12,29 +12,31 @@ var course_titles = []
 var sections = []
 //frame = document.querySelector("iframe[src*=session-tracker]").src.split("?")[1].split("=")[1]
 async function get_courses(){
-    user_courses = (await fetchApiJson(`/users/${getUserId()}/sections`)).section
+    user_courses = Array( await (await fetchApiJson(`/users/${getUserId()}/sections`)).section)
 }
 
 async function fetchCourseId(index){
+    console.log(sections.length)
     //return parseInt((await fetchApiJson(`/users/${getUserId()}/sections`)).section[index].id)
     for(i = 0; i < sections.length; i++){
-        course_ids.push(parseInt(sections[i]).id)
+        course_ids.push(parseInt( await (sections[i]).id))
     }
 }
 async function fetchCourseTitle(index){
     //return (await fetchApiJson(`/users/${getUserId()}/sections`)).section[index].course_title
     for(i = 0; i < sections.length; i++) {
-        course_titles.push(sections[i].course_title)
+        course_titles.push( await sections[i].course_title)
     }
 }
 async function fetchSections(){
-    return Array(fetchApiJson(`/users/${getUserId()}/sections`))
+    return ( await (await fetchApiJson(`/users/${getUserId()}/sections`)).section)
 }
-window.addEventListener('load', function () {
-    sections = fetchSections()
+window.addEventListener('load', async function () {
+    sections = await fetchSections()
     console.log(sections[0])
     get_courses()
-    get_course_ids()
+    await fetchCourseId()
+    await fetchCourseTitle()
     console.log(course_ids)
     console.log(course_titles)
 })
